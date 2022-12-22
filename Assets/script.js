73603a1b1476a8ce58ec7c9d1cae11b981719983
Thomas Newnum <thomas.newnum@gmail.com>
2022-12-22 06:41:34 -0500

    added other time blocks, and clock working at the top

--- Assets/script.js
+++ Assets/script.js
@@ -14,10 +14,27 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+    //an if statement that loops through if time is 
+      // if hour on schedule > current hour  make class = future
+      // if time < current hour  make class = past
+      // if time = current hour  make class = present
+
+
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
+      // pull info from id = "hour-x"
+
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+let timeDate = dayjs().format('MMMM DD, YYYY h:mm a');
+$('#date-time').text(timeDate);
+function timeUpkeep () {
+    setInterval()
+}
