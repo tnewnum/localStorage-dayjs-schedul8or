3f22fc74edb68882f180e7cfa5a21571cb06e19b
Thomas Newnum <thomas.newnum@gmail.com>
2022-12-22 09:42:38 -0500

    interanalStorage is working, only one button saves though

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,37 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+let timeDate = dayjs().format('MMMM DD, YYYY h:mm a');
+let description = document.querySelector('.description');
+const saveButton = document.querySelector('button');
+
+$('#date-time').text(timeDate);
+function timeUpkeep () {
+    setInterval()
+}
+
+
+
+saveButton.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  let nine = document.querySelector(".nine").value;
+
+      localStorage.setItem("9am", nine);      
+
+ console.log("this is 9am test")
+
+});
+
+saveButton.addEventListener('click', (event) => {
+  event.preventDefault()
+
+    let ten = document.querySelector(".ten").value;
+      localStorage.setItem("10am", ten);       
+
+ console.log("this is 10am test")
+
+});
+
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -8,6 +39,10 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+
+
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -33,8 +68,4 @@ $(function () {
   // TODO: Add code to display the current date in the header of the page.
 });
 
-let timeDate = dayjs().format('MMMM DD, YYYY h:mm a');
-$('#date-time').text(timeDate);
-function timeUpkeep () {
-    setInterval()
-}
+
