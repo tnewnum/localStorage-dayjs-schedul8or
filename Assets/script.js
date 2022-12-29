85818dac68a01962be6f931e3bbd57a2331d550c
Thomas Newnum <thomas.newnum@gmail.com>
2022-12-28 20:51:34 -0500

    started to work on color change

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,4 @@
 let timeDate = dayjs().format('MMMM DD, YYYY HH:mm a');
-let currentTime = dayjs().format('HH')
-console.log(currentTime)
-let saveButton = document.querySelectorAll('#save');
-let description = document.querySelectorAll('.description');
-
 
 $('#date-time').text(timeDate);
 function timeUpkeep () {
@@ -11,8 +6,10 @@ function timeUpkeep () {
 }
 
 
-for (let i = 0; i < saveButton.length; i++) {
 
+let saveButton = document.querySelectorAll('#save');
+for (let i = 0; i < saveButton.length; i++) {
+  
   saveButton[i].addEventListener('click', function(event) {
     event.preventDefault()
 
@@ -30,32 +27,51 @@ for (let i = 0; i < saveButton.length; i++) {
 };
 
 function writeItems(item) {
-  // let description = document.querySelectorAll('.description')
+  let description = document.querySelectorAll('.description')
+
   item = JSON.parse(localStorage.getItem('item0'))
-console.log(item)
+    console.log(item)
 
-  for (let i = 0; i < item.length; i++){
-
+    for (let i = 0; i < item.length; i++){
 
     description[i].value = item[i]
     
-  }
+    }
 } ;
 
 writeItems()
 
+ //an if statement that loops through if time is 
+//       // if hour on schedule > current hour  make class = future
+//       // if time < current hour  make class = past
+//       // if time = current hour  make class = presen
+
+function changeColor () {
+
+  let currentTime = dayjs().format('HH')
+  console.log(currentTime)
+
+  let hour = $('#hour')
+
+  for ( let i = 0; i < hour.length; i++){
 
 
-// function changeColor () {
-//  let nine = document.getElementById('#hour-9')
-//  let ten = document.getElementById('#hour-10')
-//  let eleven = document.getElementById('#hour-11')
-//  let twelve = document.getElementById('#hour-12')
-//  let thirteen = document.getElementById('#hour-13')
-//  let fourteen = document.getElementById('#hour-14')
-//  let fifteen = document.getElementById('#hour-15')
-//  let sixteen = document.getElementById('#hour-16')
-//  let seventeen = document.getElementById('#hour-17')
+  }
+
+}
+changeColor();
+
+
+
+//  let nine = $('#hour-9')
+//  let ten = $('#hour-10')
+//  let eleven = $('#hour-11')
+//  let twelve = $('#hour-12')
+//  let thirteen = $('#hour-13')
+//  let fourteen = $('#hour-14')
+//  let fifteen = $('#hour-15')
+//  let sixteen = $('#hour-16')
+//  let seventeen = $('#hour-17')
 //  console.log(nine);
 
 //  if(currentTime > 9 ) {
@@ -121,8 +137,7 @@ writeItems()
 //   seventeen.classList.remove('present')
 //   seventeen.classList.add('future')
 //  }
-// }
-// changeColor();
+
 
 
 // $(function () {
@@ -143,21 +158,13 @@ writeItems()
 //   // past, present, and future classes? How can Day.js be used to get the
 //   // current hour in 24-hour time?
 
-//     //an if statement that loops through if time is 
-//       // if hour on schedule > current hour  make class = future
-//       // if time < current hour  make class = past
-//       // if time = current hour  make class = present
-
-
 
 //   //
 //   // TODO: Add code to get any user input that was saved in localStorage and set
 //   // the values of the corresponding textarea elements. HINT: How can the id
 //   // attribute of each time-block be used to do this?
 
-//       // pull info from id = "hour-x"
 
-//   //
 //   // TODO: Add code to display the current date in the header of the page.
 // });
 
