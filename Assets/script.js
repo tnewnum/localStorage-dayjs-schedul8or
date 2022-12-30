45c7bf8da49c7c7af8ff5926b95355d2ba0fc9a1
Thomas Newnum <thomas.newnum@gmail.com>
2022-12-30 17:12:44 -0500

    all code appears to be working. need to put inside of one function, and comments

--- Assets/script.js
+++ Assets/script.js
@@ -6,7 +6,48 @@ function timeUpkeep () {
 }
 
 
+function changeColor () {
+  let currentTime = dayjs().format('HH')
+  console.log(currentTime)
 
+  let hour = $('.row')
+
+  console.log(hour)
+
+  for ( let i = 0; i < hour.length; i++){
+
+    let time = $(hour[i]).attr("id")
+    console.log(time)
+
+    // for (let i = 0; i < time.length; i++)
+      
+
+        if (time < currentTime) {
+      
+          $(hour[i]).addClass('past')
+
+          // change class  past
+        }
+        
+        if (time > currentTime) {
+
+          $(hour[i]).addClass('future')
+
+          // change class future 
+        } 
+        
+        if (time === currentTime) { 
+
+          $(hour[i]).addClass('present')
+        
+          // change class present
+
+        }
+    }
+}
+changeColor();
+
+function saveItems (){
 let saveButton = document.querySelectorAll('#save');
 let description = document.querySelectorAll('.description')
 
@@ -26,10 +67,11 @@ for (let i = 0; i < saveButton.length; i++) {
     localStorage.setItem('item0', JSON.stringify(item))
 
   })
-};
+}};
+saveItems()
 
 function writeItems(item) {
- 
+  let description = document.querySelectorAll('.description')
 
   item = JSON.parse(localStorage.getItem('item0'))
     console.log(item)
@@ -43,134 +85,79 @@ function writeItems(item) {
 
 writeItems()
 
- //an if statement that loops through if time is 
-//       // if hour on schedule > current hour  make class = future
-//       // if time < current hour  make class = past
-//       // if time = current hour  make class = presen
+
 
 // function changeColor () {
 
 //   let currentTime = dayjs().format('HH')
 //   console.log(currentTime)
 
-//   let hour = $('#hour')
+//   let hour = $('.row')
 
 //   for ( let i = 0; i < hour.length; i++){
 
-//     if hour.innerText < currentTime s
-//     if hour.innerText > currentTime changeColor
-//     if hour.innerText == currentTime changeColor
 
+//     let time = $(hour[i]).attr("id")
+
+//     for (let i = 0; i < time.length; i++)
+
+//       console.log(time)
+
+//         if (time < currentTime) {
+      
+//           $('.row').addClass('past')
+
+//           // change class  past
+
+//         } else if (time > currentTime) {
+
+//           $('row').addClass('future')
+
+//           // change class future 
+
+//         } else if (time == currentTime) { 
+
+//           $('.row').addClass('present')
+        
+//           // change class present
+
+//     }
 //   }
-
 // }
 // changeColor();
 
 
 
-//  let nine = $('#hour-9')
-//  let ten = $('#hour-10')
-//  let eleven = $('#hour-11')
-//  let twelve = $('#hour-12')
-//  let thirteen = $('#hour-13')
-//  let fourteen = $('#hour-14')
-//  let fifteen = $('#hour-15')
-//  let sixteen = $('#hour-16')
-//  let seventeen = $('#hour-17')
-//  console.log(nine);
-
-//  if(currentTime > 9 ) {
-//   nine.classList.remove('present')
-//   nine.classList.add('past')
-//  } else if (currentTime < 9) {
-//   nine.classList.remove('present')
-//   nine.classList.add('future')
-//  }
-//  if(currentTime > 10) {
-//   ten.classList.remove('present')
-//   ten.classList.add('past')
-//  } else if (currentTime < 10) {
-//   ten.classList.remove('present')
-//   ten.classList.add('future')
-//  }
-//  if(currentTime > 11) {
-//   eleven.classList.remove('present')
-//   eleven.classList.add('past')
-//  } else if (currentTime < 11) {
-//   eleven.classList.remove('present')
-//   eleven.classList.add('future')
-//  }
-//  if(currentTime > 12) {
-//   twelve.classList.remove('present')
-//   twelve.classList.add('past')
-//  } else if (currentTime < 12) {
-//   twelve.classList.remove('present')
-//   twelve.classList.add('future')
-//  }
-//  if(currentTime > 13) {
-//   thirteen.classList.remove('present')
-//   thirteen.classList.add('past')
-//  } else if (currentTime < 13) {
-//   thirteen.classList.remove('present')
-//   thirteen.classList.add('future')
-//  }
-//  if(currentTime > 14) {
-//   fourteen.classList.remove('present')
-//   fourteen.classList.add('past')
-//  } else if (currentTime < 14) {
-//   fourteen.classList.remove('present')
-//   fourteen.classList.add('future')
-//  }
-//  if(currentTime > 15) {
-//   fifteen.classList.remove('present')
-//   fifteen.classList.add('past')
-//  } else if (currentTime < 15) {
-//   fifteen.classList.remove('present')
-//   fifteen.classList.add('future')
-//  }
-//  if(currentTime > 16) {
-//   sixteen.classList.remove('present')
-//   sixteen.classList.add('past')
-//  } else if (currentTime < 16) {
-//   sixteen.classList.remove('present')
-//   sixteen.classList.add('future')
-//  }
-//  if(currentTime > 17) {
-//   seventeen.classList.remove('present')
-//   seventeen.classList.add('past')
-//  } else if (currentTime < 17) {
-//   seventeen.classList.remove('present')
-//   seventeen.classList.add('future')
-//  }
-
-
-
-// $(function () {
-//   // TODO: Add a listener for click events on the save button. This code should
-//   // use the id in the containing time-block as a key to save the user input in
-//   // local storage. HINT: What does `this` reference in the click listener
-//   // function? How can DOM traversal be used to get the "hour-x" id of the
-//   // time-block containing the button that was clicked? How might the id be
-//   // useful when saving the description in local storage?
 
 
 
 
-//   //
-//   // TODO: Add code to apply the past, present, or future class to each time
-//   // block by comparing the id to the current hour. HINTS: How can the id
-//   // attribute of each time-block be used to conditionally add or remove the
-//   // past, present, and future classes? How can Day.js be used to get the
-//   // current hour in 24-hour time?
+$(function () {
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
 
 
-//   //
-//   // TODO: Add code to get any user input that was saved in localStorage and set
-//   // the values of the corresponding textarea elements. HINT: How can the id
-//   // attribute of each time-block be used to do this?
 
 
-//   // TODO: Add code to display the current date in the header of the page.
-// });
+  //
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+
+
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+
+
+  // TODO: Add code to display the current date in the header of the page.
+});
 
 
